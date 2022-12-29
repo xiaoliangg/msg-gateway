f6af0e34e57e8a02b3769016414ad5c49f57249a
yuliang <yl@yl.com>
2022-12-29 09:43:09 +0800

    集成http-proxy,普通http连接代理测试通过

--- server/app.js
+++ server/app.js
@@ -7,14 +7,34 @@ const hostname = '127.0.0.1';
 const port = process.env.PORT | 3000
 const environment = process.env.NODE_ENV
 
-const server = http.createServer((req, res) => {
+const server2 = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end('Hello World');
 });
 
-server.listen(port, hostname, () => {
+server2.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
     // 当启用test 和 start时,输出的环境不一样，测试通过
     console.log(`Server running environment: ${environment}/`);
 });
+
+
+
+var httpProxy = require('http-proxy');
+var options = {
+}
+var proxy = httpProxy.createProxyServer(options);
+var server = http.createServer(function(req, res) {
+    // You can define here your custom logic to handle the request
+    // and then proxy the request.
+    const data = req.body
+    const params = req.query
+
+    console.log(`data: ${data}/params: ${params}/`);
+    console.log(req);
+
+    proxy.web(req, res, { target: 'http://127.0.0.1:3000' });
+});
+console.log("listening on port 5050")
+server.listen(5051);
