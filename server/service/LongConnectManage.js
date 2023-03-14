abc6321cd4210be7836646d0a0298a39aafc0de3
xiaoliangg <1535523313@qq.com>
2023-03-14 11:28:12 +0800

    接收客户端长连接

--- server/service/LongConnectManage.js
+++ server/service/LongConnectManage.js
@@ -10,7 +10,7 @@ import * as CONST from '../service/CONST'
  * @returns {Promise<unknown>}
  */
 export const addLongConnect = async data => {
-  console.log(`add new long connect,nid:${nid},uid:${uid}`)
+  console.log(`add new long connect,nid:${data.nid},uid:${data.uid}`)
   await myRedis.client.sAdd(CONST.SERVER_ONLINE_UIDS(data.nid),data.uid);
   await myRedis.client.set(CONST.SERVER_ONLINE_BELONG_NID(data.uid),data.nid);
   await myRedis.client.zIncrBy(CONST.SERVER_SEND, 1, data.nid)
@@ -18,14 +18,14 @@ export const addLongConnect = async data => {
 
 // 节点释放长连接
 export const deleteLongConnect = async data => {
-  console.log(`delete new long connect,nid:${nid},uid:${uid}`)
+  console.log(`delete new long connect,nid:${data.nid},uid:${data.uid}`)
   await myRedis.client.sRem(CONST.SERVER_ONLINE_UIDS(data.nid),data.uid);
   await myRedis.client.del(CONST.SERVER_ONLINE_BELONG_NID(data.uid));
   var zRank = await myRedis.client.zRank(CONST.SERVER_SEND,data.nid);
   if(zRank){ // 如果不存在,zRank会返回null
     await myRedis.client.zIncrBy(CONST.SERVER_SEND, -1, data.nid)
   }else{
-    console.log(`${data.nid} null,无需减1`)
+    console.log(`${data.nid} is null,don't decrease score`)
   }
 }
 
